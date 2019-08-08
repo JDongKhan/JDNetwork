eadaccb0ed8a19a25dfc86c29e6f34f008316b30
JD <419591321@qq.com>
2019-08-08 11:38:47 +0800

    add comment

--- Framework/Interceptor/JDNetworkInterceptor.h
+++ Framework/Interceptor/JDNetworkInterceptor.h
@@ -9,6 +9,10 @@
 #import <Foundation/Foundation.h>
 #import "JDNetworkChain.h"
 
+/**
+ 这块不好设计，因为AF的请求是异步的导致很多流程无法窜起来，等后续有好的想法再优化
+
+ */
 NS_ASSUME_NONNULL_BEGIN
 
 @protocol JDNetworkInterceptor <NSObject>
